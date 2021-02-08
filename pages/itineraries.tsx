8fca6a92e0aac6e75877b324bda0313149177b95
TitouanCao <titouan.minier.mancini@gmail.com>
2021-02-08 23:43:40 +0100

    gitignore fix

--- pages/itineraries.tsx
+++ pages/itineraries.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Layout } from "../components"
 
-export default function Route() {
+export default function Itineraries() {
     return (
         <Layout>
             <div className="default_centered_div">
